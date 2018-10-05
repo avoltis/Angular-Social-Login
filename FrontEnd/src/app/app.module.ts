d4f1e4645a1497a90e06faaa0bde4c101a210558
vagolli <v.agolli@live.com>
2018-10-05 10:04:42 +0200

    routing

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { ApiService} from './api.service';
+import { ApiService } from './api.service';
 import { MessagesComponent } from './messages.component';
+import { RegisterComponent } from './register.component';
+
+const routes = [
+    {path: 'register', component: RegisterComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent
+    MessagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
