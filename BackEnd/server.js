13943a8a935b62e881a4f020c525bae97d5d4423
vagolli <v.agolli@live.com>
2018-10-16 10:36:01 +0200

    auth router

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,11 +2,10 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var jwt = require('jwt-simple');
 var app = express();
-var bcrypt = require('bcrypt-nodejs');
 
 var User = require('./models/User.js');
+var auth = require('./auth.js');
 
 mongoose.Promise = Promise;
 
@@ -46,41 +45,6 @@ app.get('/profile/:id', async (req, res) => {
     }
 });
 
-app.post('/register', (req, res) => {
-    var userData = req.body;
-
-    var user = new User(userData);
-    user.save((err, result) => {
-        if (err) {
-            console.log('saving user error');
-        }
-        res.sendStatus(200);
-    });
-
-    console.log(userData.email);
-});
-
-app.post('/login', async (req, res) => {
-    var loginData = req.body;
-
-    var user = await User.findOne({ email: loginData.email });
-    if (!user) {
-        return res.status(401).send({ message: 'Email or password is invalid' })
-    }
-
-    bcrypt.compare(loginData.pwd, user.pwd, (err, isMatch) => {
-        if (!isMatch) {
-            return res.status(401).send({ message: 'Email or password is invalid' })
-        }
-
-        var payload = {};
-
-        var token = jwt.encode(payload, '123') // "123" secret for encoding must be put into configs
-
-        res.status(200).send({ token });
-    });
-
-});
 
 mongoose.connect('mongodb://test:test12@ds119993.mlab.com:19993/pssocial', { useNewUrlParser: true }, (err) => {
     if (!err) {
@@ -88,4 +52,5 @@ mongoose.connect('mongodb://test:test12@ds119993.mlab.com:19993/pssocial', { use
     }
 });
 
+app.use('/auth', auth);
 app.listen(3000);
